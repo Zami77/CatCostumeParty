57c88779d90c07f5237f3ae4ed84ce9a5cc25269
Dan Lungaro <Chunk_77@hotmail.com>
2023-10-28 10:29:15 -0700

    game manager and main menu impl

--- CatCostumeParty/common/ScenePaths.gd
+++ CatCostumeParty/common/ScenePaths.gd
@@ -1,6 +1,9 @@
 class_name ScenePaths
 extends Node
 
+static var main_menu = "res://ui/main_menu/MainMenu.tscn"
+static var match_manager = "res://managers/MatchManager.tscn"
+
 static var costume_piece = "res://costume_pieces/CostumePiece.tscn"
 static var costume_cat = "res://costume_cats/CostumeCat.tscn"
 static var dracula_costume_cat = "res://costume_cats/dracula_cat/DraculaCostumeCat.tscn"
---
+++ CatCostumeParty/ui/main_menu/MainMenu.gd
@@ -0,0 +1,20 @@
+class_name MainMenu
+extends Node2D
+
+signal option_selected(option: Option)
+
+@onready var play_1p_button = $VBoxContainer/Play1pButton
+@onready var play_2p_button = $VBoxContainer/Play2pButton
+@onready var instructions_button = $VBoxContainer/InstructionsButton
+@onready var credits_button = $VBoxContainer/CreditsButton
+
+enum Option { PLAY_1P, PLAY_2P, INSTRUCTIONS, CREDITS }
+
+func _ready():
+	play_1p_button.pressed.connect(_on_button_pressed.bind(Option.PLAY_1P))
+	play_2p_button.pressed.connect(_on_button_pressed.bind(Option.PLAY_2P))
+	instructions_button.pressed.connect(_on_button_pressed.bind(Option.INSTRUCTIONS))
+	credits_button.pressed.connect(_on_button_pressed.bind(Option.CREDITS))
+
+func _on_button_pressed(option: Option) -> void:
+	emit_signal("option_selected", option)
