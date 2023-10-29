b9de04a9ce3a93ad9a4c9ec6d1bf3462fe11465e
Daniel Lungaro <chunk_77@hotmail.com>
2023-10-28 23:40:48 -0700

    import wizard and dracula cat plus prototype

--- CatCostumeParty/common/CostumePieceTextures.gd
+++ CatCostumeParty/common/CostumePieceTextures.gd
@@ -1,6 +1,14 @@
 class_name CostumePieceTextures
 extends Node
 
-static var broom: Texture = load("res://costume_pieces/Broom_Prototype.png")
-static var fangs: Texture = load("res://costume_pieces/fangs_prototype.png")
-static var cloak: Texture = load("res://costume_pieces/cloak_prototype.png")
+static var broom: Texture = preload("res://costume_pieces/costume_piece_art/wizard_broom1.png")
+static var wand: Texture = preload("res://costume_pieces/costume_piece_art/wizard_ wand1.png")
+static var wizard_hat: Texture = preload("res://costume_pieces/costume_piece_art/wizard_hat.png")
+
+static var eye_patch: Texture = preload("res://costume_pieces/costume_piece_art/pirate_eyepatch1.png")
+static var hook: Texture = preload("res://costume_pieces/prototype_costume_pieces/hook_prototype.png")
+static var pirate_hat: Texture = preload("res://costume_pieces/prototype_costume_pieces/pirate_hat_prototype.png")
+
+static var fangs: Texture = preload("res://costume_pieces/prototype_costume_pieces/fangs_prototype.png")
+static var cloak: Texture = preload("res://costume_pieces/prototype_costume_pieces/cloak_prototype.png")
+static var goblet: Texture = preload("res://costume_pieces/prototype_costume_pieces/goblet_prototype.png")
--- CatCostumeParty/costume_pieces/CostumePiece.gd
+++ CatCostumeParty/costume_pieces/CostumePiece.gd
@@ -1,7 +1,7 @@
 class_name CostumePiece
 extends Area2D
 
-enum CostumeComponent { CLOAK, FANGS, BROOM }
+enum CostumeComponent { CLOAK, FANGS, BROOM, EYE_PATCH, HOOK, PIRATE_HAT, WIZARD_HAT, WAND, GOBLET }
 
 signal piece_selected(selected_piece: CostumePiece)
 
@@ -26,6 +26,18 @@ func _load_costume_piece_image():
 			texture_rect.texture = CostumePieceTextures.fangs
 		CostumeComponent.BROOM:
 			texture_rect.texture = CostumePieceTextures.broom
+		CostumeComponent.WIZARD_HAT:
+			texture_rect.texture = CostumePieceTextures.wizard_hat
+		CostumeComponent.WAND:
+			texture_rect.texture = CostumePieceTextures.wand
+		CostumeComponent.EYE_PATCH:
+			texture_rect.texture = CostumePieceTextures.eye_patch
+		CostumeComponent.HOOK:
+			texture_rect.texture = CostumePieceTextures.hook
+		CostumeComponent.PIRATE_HAT:
+			texture_rect.texture = CostumePieceTextures.pirate_hat
+		CostumeComponent.GOBLET:
+			texture_rect.texture = CostumePieceTextures.goblet
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
